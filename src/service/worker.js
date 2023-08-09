61998f84d04f664121f5516c640bf0da49b42185
NuttakitDW <nuttakit@roostoo.com>
2023-08-09 20:31:40 +0700

    optimize check profile

--- src/service/worker.js
+++ src/service/worker.js
@@ -7,18 +7,16 @@ const closeAllPosition = require("../usecase/closeallposition");
 const pushMessage = require("./pushmessage");
 require("dotenv").config();
 
+let nonActiveCount = 0;
+const DAY = 1440; // Day in min unit
+
 async function startWorker() {
   try {
     const name = process.env.TRADER;
     const trader = await getTrader(name);
     const positions = await getPosition(trader.encryptedUid);
-    const info = await getInfo(trader.encryptedUid);
 
-    if (!info.positionShared) {
-      const msg = `${name} no longer share position.`;
-      console.log(msg);
-      return;
-    }
+    console.log(`nonActiveCount: ${nonActiveCount}`);
 
     if (positions === null && Object.keys(trader.positions).length > 0) {
       const msgs = await closeAllPosition(trader);
@@ -26,6 +24,19 @@ async function startWorker() {
         pushMessage(msgs[i]);
       }
     } else if (positions === null) {
+      nonActiveCount += 1;
+      timePass = process.env.INTERVAL * nonActiveCount;
+
+      // check profile only first null position and when trader not active more than 1 day
+      if (nonActiveCount === 1 || timePass > DAY) {
+        timePass > DAY ? nonActiveCount = 0 : nonActiveCount;
+
+        const info = await getInfo(trader.encryptedUid);
+        if (!info.positionShared) {
+          console.error(`${name} no longer share position.`);
+          process.exit(1); 
+        }
+      }
       const now = Date.now();
       const thNow = unixTimeToLocalThaiDateTime(now);
       console.log(`${name} doesn't have any position yet.`);
