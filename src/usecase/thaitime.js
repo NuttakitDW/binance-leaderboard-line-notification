ec8eeb4fa826da176b0bd9c22b23161b44d98fa7
NuttakitDW <nuttakit@roostoo.com>
2023-08-09 20:06:39 +0700

    restructure time function

--- src/usecase/thaitime.js
+++ src/usecase/thaitime.js
@@ -3,20 +3,20 @@
  * @returns {string}
  */
 function unixTimeToLocalThaiDateTime(unixTimestamp) {
-    const date = new Date(unixTimestamp); // Convert Unix timestamp to milliseconds
-  
-    const options = {
-      year: "numeric",
-      month: "2-digit",
-      day: "2-digit",
-      hour: "2-digit",
-      minute: "2-digit",
-      second: "2-digit",
-      hour12: false,
-      timeZone: "Asia/Bangkok",
-    };
-  
-    return date.toLocaleString("th-TH", options);
-  }
+  const date = new Date(unixTimestamp); // Convert Unix timestamp to milliseconds
 
-  module.exports = unixTimeToLocalThaiDateTime;
+  const options = {
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+    hour12: false,
+    timeZone: "Asia/Bangkok",
+  };
+
+  return date.toLocaleString("th-TH", options);
+}
+
+module.exports = unixTimeToLocalThaiDateTime;
--- src/usecase/timediff.js
+++ src/usecase/timediff.js
@@ -9,23 +9,4 @@ function timeDiffFromUnixTimestamp(unixTimestamp) {
     return diffInSeconds
 }
 
-/**
- * @param {number} unixTimestamp
- * @returns {string}
- */
-function timeAgoFromUnixTimestamp(unixTimestamp) {
-    const now = Date.now(); // Convert current time to Unix timestamp in seconds
-    const diffInSeconds = Math.floor((now - unixTimestamp)/1000);
-  
-    if (diffInSeconds >= 3600) {
-      const diffInHours = Math.floor(diffInSeconds / 3600);
-      return `${diffInHours} hr${diffInHours > 1 ? 's' : ''} ago`;
-    } else if (diffInSeconds >= 60) {
-      const diffInMinutes = Math.floor(diffInSeconds / 60);
-      return `${diffInMinutes} min${diffInMinutes > 1 ? 's' : ''} ago`;
-    } else {
-      return `${diffInSeconds} sec${diffInSeconds !== 1 ? 's' : ''} ago`;
-    }
-}
-
-  module.exports = timeAgoFromUnixTimestamp, timeDiffFromUnixTimestamp;
+  module.exports = timeDiffFromUnixTimestamp;
